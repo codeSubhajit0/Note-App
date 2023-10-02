a95ca090b2700cbf86a4d754cfbb8f3561339347
Subhajit Karmakar <subhajitk731@gmail.com>
2023-10-03 00:55:40 +0530

    Color selector complete using html and js

--- note.js
+++ note.js
@@ -2,13 +2,36 @@
 
 const createBtn = document.querySelector("#newbTn");
 const noteForm = document.querySelector(".Nform");
+const crossbtn = document.querySelector('.crossBtn')
+
+const heading = document.querySelector('.heading')
+const textNote = document.querySelector('.textNote')
+const createbTn = document.querySelector('.createbTn')
+const colorSlide = document.querySelector('.colorSlide')
+
 
 createBtn.addEventListener("click", function (e) {
-  // console.log(e.target === createBtn|| e.target.closest('#newbTn') === createBtn);
-
   if (!(e.target === createBtn || e.target.closest("#newbTn") === createBtn))
     return;
   // console.log('hello');
-
-  noteForm.classList.toggle("Nform_active");
+  noteForm.classList.add("Nform_active");
 });
+
+crossbtn.addEventListener('click',function(e){
+  if(!(e.target === crossbtn || e.target.closest('.crossBtn')))
+  return;
+  noteForm.classList.remove("Nform_active");
+});
+
+colorSlide.addEventListener('click',function(e){
+  if( !(e.target.classList[0] === 'color')) return;
+  let color = e.target.dataset.num;
+  crossbtn.style.backgroundColor = `var(--color-${color})`;
+  heading.style.backgroundColor = `var(--color-${color})`;
+  textNote.style.backgroundColor = `var(--color-${color})`;
+  createbTn.style.backgroundColor = `var(--color-${color})`;
+})
+
+createbTn.addEventListener('click',function(e){
+
+})

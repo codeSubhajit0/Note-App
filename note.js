4eb7d3d56dfc6f5e533ccab5520c282855553167
Subhajit Karmakar <subhajitk731@gmail.com>
2023-10-16 23:08:00 +0530

    Added the functionality that only my newly added notes will appear at the begining and the note will be stored at the localstorage

--- note.js
+++ note.js
@@ -2,86 +2,95 @@
 
 const newBtn = document.querySelector("#newbTn");
 const noteForm = document.querySelector(".Nform");
-const crossbtn = document.querySelector('.crossBtn')
+const crossbtn = document.querySelector(".crossBtn");
 
-const heading = document.querySelector('.heading')
-const textNote = document.querySelector('.textNote')
-const createbTn = document.querySelector('.createbTn')
-const colorSlide = document.querySelector('.colorSlide')
+const heading = document.querySelector(".heading");
+const textNote = document.querySelector(".textNote");
+const createbTn = document.querySelector(".createbTn");
+const colorSlide = document.querySelector(".colorSlide");
 
-const previos = document.querySelector('.notes');
-
-
+const previos = document.querySelector(".notes");
+let note;
 let colorMain = "var(--color-1)";
 
+const notes = [];
+
+class App {
+  constructor(title, text, color) {
+    this.title = title;
+    this.text = text;
+    this.color = color;
+  }
+}
+
+// This is used for initial loding of notes and add some data to local storage
+const initial = function () {
+  note = JSON.parse(localStorage.getItem("notes"));
+  note.forEach((element, index) => {
+    const newSection = document.createElement("section");
+    newSection.classList.add("note");
+    newSection.textContent = `${element.title}`;
+    previos.insertBefore(newSection, previos.children[0]);
+    newSection.style.backgroundColor = element.color;
+  });
+};
+initial();
+
+// Opening the new note form 
 newBtn.addEventListener("click", function (e) {
-  if (!(e.target === newBtn || e.target.closest("#newbTn") === newBtn))
-    return;
+  if (!(e.target === newBtn || e.target.closest("#newbTn") === newBtn)) return;
   // console.log('hello');
   noteForm.classList.add("Nform_active");
 });
 
-crossbtn.addEventListener('click',function(e){
-  if(!(e.target === crossbtn || e.target.closest('.crossBtn')))
-  return;
+// Closing the note form
+crossbtn.addEventListener("click", function (e) {
+  if (!(e.target === crossbtn || e.target.closest(".crossBtn"))) return;
   noteForm.classList.remove("Nform_active");
 });
 
-colorSlide.addEventListener('click',function(e){
-  if( !(e.target.classList[0] === 'color')) return;
+// Changed the background color of the note
+colorSlide.addEventListener("click", function (e) {
+  if (!(e.target.classList[0] === "color")) return;
   let color = e.target.dataset.num;
   colorMain = `var(--color-${color})`;
   crossbtn.style.backgroundColor = `var(--color-${color})`;
   heading.style.backgroundColor = `var(--color-${color})`;
   textNote.style.backgroundColor = `var(--color-${color})`;
   createbTn.style.backgroundColor = `var(--color-${color})`;
-})
+});
 
+const setItemToLocalstoraeg = function () {
+  localStorage.setItem("notes", JSON.stringify(notes));
+};
 
-const notes = [];
-
-class App {
-  constructor(title,text,color){
-    this.title = title;
-    this.text = text;
-    this.color = color;
-  }
-
-}
-
-const setItemToLocalstoraeg = function(){
-  localStorage.setItem("notes",JSON.stringify(notes))
-}
-
-const getItemFromLocalstorage = function(){
-  const note = JSON.parse(localStorage.getItem('notes'));
+const getItemFromLocalstorage = function () {
+  note = JSON.parse(localStorage.getItem("notes"));
   console.log(note);
   prevNotes(note);
-}
+};
 
-const prevNotes = function (notes){
+const prevNotes = function (notes) {
   // previos.insertAdjacentHTML = `<section class="note">${note[0].title}</section>`;
-  const newSection = document.createElement("section");
-  newSection.classList.add('note');
-  newSection.textContent = `${notes[0].title}`;
-  previos.insertBefore(newSection,previos.children[0]);
-  newSection.style.backgroundColor = notes[0].color ;
-}
+  notes.forEach((element, index) => {
+    if (index == notes.length - 1) {
+      const newSection = document.createElement("section");
+      newSection.classList.add("note");
+      newSection.textContent = `${element.title}`;
+      previos.insertBefore(newSection, previos.children[0]);
+      newSection.style.backgroundColor = element.color;
+    }
+  });
+};
 
 // Functionality of the create button
-createbTn.addEventListener('click',function(e){
-    // console.log('hi');
-    let NoteHeading = heading.value;
-    let NoteContent = textNote.value;
-    console.log(NoteContent);
-    heading.value = "";
-    textNote.value = "";
-    notes.push(new App(NoteHeading,NoteContent,colorMain));
-    console.log(setItemToLocalstoraeg());
-    getItemFromLocalstorage();
-    // console.log(notes[0].text);
-    
-    // console.log(window.getComputedStyle(textNote).background);
-    
-})
-
+createbTn.addEventListener("click", function (e) {
+  let NoteHeading = heading.value;
+  let NoteContent = textNote.value;
+  console.log(NoteContent);
+  heading.value = "";
+  textNote.value = "";
+  notes.push(new App(NoteHeading, NoteContent, colorMain));
+  setItemToLocalstoraeg();
+  getItemFromLocalstorage();
+});
